105b54f735a244570efc78db627af3ca2e6f69cd
woochanlee <030219woo@naver.com>
2020-01-16 19:39:29 +0900

    delete component

--- src/components/Main/HomeWorkBoardItem.js
+++ src/components/Main/HomeWorkBoardItem.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import oIcon from '../../imgs/Main/oIcon.png';
-import xIcon from '../../imgs/Main/xIcon.png';
+
 import { HomeWorkBoardItemBlock } from './styles';
 
 const HomeWorkBoardItem = ({ data }) => {
     return (
-        <HomeWorkBoardItemBlock>
-            <th>{data.type}</th>
-            <th>{data.title}</th>
-            <th>{data.date}</th>
-            <th>{data.endDate}</th>
-            <th>{data.views}</th>
-            <th><img src={data.team ? oIcon : xIcon} /></th>
-            <th><img src={data.submit ? oIcon : xIcon} /></th>
-        </HomeWorkBoardItemBlock>
+        <></>
     );
 };
 
