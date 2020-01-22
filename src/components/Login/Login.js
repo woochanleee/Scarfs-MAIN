05ba184470c95c2c77856ab60c8a815c4ee3719b
leewoochan <030219woo@naver.com>
2020-01-23 01:36:37 +0900

    update: sliding animation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import * as S from '../SignUp/styles';
 import blackXButton from '../../Imgs/SignUp/blackXButton.png';
 
-const Login = () => {
+const Login = ({ modalOn, setModalOn }) => {
+    const [authorization, setAutorization] = useState(false);
+    const [sliding, setSliding] = useState(false);
+    const [isPaint, setIsPaint] = useState(false);
+    const paintNextButton = () => {
+        setIsPaint(true);
+        setTimeout(() => {
+            setIsPaint(false);
+        }, 1000);
+    };
+    useEffect(() => {
+        setTimeout(() => {
+            setSliding(true);
+        }, 1)
+    }, []);
     return (
         <S.ModalBackground>
-            <S.LOGINSIGNUPWrapper>                
+            <S.LOGINSIGNUPWrapper sliding={sliding}>             
                 <S.LoginLeftBlock>
                     <S.MainWrapper>
                         <h1>LOGIN</h1>
@@ -29,12 +43,13 @@ const Login = () => {
                     <h4>2020 SCIENCE CLASS</h4>
                 </S.LoginLeftBlock>
                 <S.LoginRightBlock>
-                    <img src={blackXButton} />
+                    <img src={blackXButton} onClick={() => setModalOn({ ...modalOn, login: false })} />
                     <div>
                         <h1>아직 회원이 아니신가요?<br/>
                         지금 바로 가입하세요! </h1>
-                        <div>
-                            <Link to="/signup/" >회원가입</Link>
+                        <div onClick={() => setModalOn({ ...modalOn, login: false, signup: true })}>
+                            {/* <Link to="/signup/" >회원가입</Link> */}
+                            <span>회원가입</span>
                         </div>
                     </div>
                     <h4>SCARFS</h4>
