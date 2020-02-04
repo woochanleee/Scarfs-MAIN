2ed99544bda8c6741c597b595866e0851ccec5a0
leewoochan <030219woo@naver.com>
2020-02-04 23:34:21 +0900

    update: change server base url

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
-const url = "https://virtserver.swaggerhub.com/LeagueLugas1/scarfs/1.0.0";
+const url = "http://15.164.184.104:8888";
 const ApiDefault = axios.create({ baseURL : url });
+
 export default ApiDefault;
+
 export const IssuingToken = async () => {
     return await ApiDefault.put('/auth', {
         headers: { Authorization: "Bearer " + localStorage.getItem('refresh_token') }
