6ec72c95b0412ac726024e0437f3577380d4d4c5
leewoochan <030219woo@naver.com>
2020-02-04 23:33:44 +0900

    delete: not need

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Link } from 'react-router-dom';
 import * as S from '../SignUp/styles';
 import blackXButton from '../../Imgs/SignUp/blackXButton.png';
 import ApiDefault, { IssuingToken } from '../utils';
