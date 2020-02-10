f42898dd6ed3eebf43cd1b47d02d1282b2b7af65
leewoochan <030219woo@naver.com>
2020-02-10 23:52:22 +0900

    [ADD] <utils> ApiDefault + IssuingToken

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
-const url = "http://15.164.184.104:8888/";
-const ApiDefault = axios.create({ 
-    baseURL : url, 
-    headers: {
-    'Access-Control-Allow-Origin': '*'
-} });
+const url = "http://54.180.174.253:8888/";
+const ApiDefault = axios.create({ baseURL : url });
 
 export default ApiDefault;
 
 export const IssuingToken = async () => {
-    return await ApiDefault.put('/auth', {
-        headers: { Authorization: "Bearer " + localStorage.getItem('refresh_token') }
-    })
+    try {
+        const putAuth = await ApiDefault.put('/auth', undefined, {
+            headers: { Authorization: localStorage.getItem('refresh_token') }
+        })
+        console.log(putAuth);
+        localStorage.setItem('access_token', putAuth.data.accessToken);
+        localStorage.setItem('refresh_token', putAuth.data.refreshToken);
+        return 'success';
+    } catch (e) {
+        console.log(e);
+        throw new Error(e.response.status);
+    }
 };
