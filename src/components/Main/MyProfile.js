a3993bd69daf6271e41aebbd57e258a2f3da77a2
leewoochan <030219woo@naver.com>
2020-02-12 21:04:54 +0900

    [ADD] <Main> 내 정보 서버 통신

--- src/components/Main/MyProfile.js
+++ src/components/Main/MyProfile.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MyProfileBlock } from './styles';
+import ApiDefault, { IssuingToken } from '../utils';
 const MyProfile = () => {
+    const [myInfo, setMyInfo] = useState({});
     const myProfile = {
         studentNumber: 1201,
         name: '강신희',
         submit: '4',
         noSubmit: '2'
     };
+    useEffect(() => {
+        ApiDefault.get('user', {
+            headers: {
+                Authorization: localStorage.getItem('access_token')
+            }
+        }).then(res => {
+            console.log(res);
+            setMyInfo(res.data);
+        }).catch(e => {
+            console.log(e.response);
+            // if (e.response.status === 410)
+                // IssuingToken();
+        })
+    }, []);
     return (
         <MyProfileBlock>
-            <h3>{myProfile.studentNumber}{myProfile.name}</h3>
+            <h3>{myInfo.userNumber}{myInfo.userName}</h3>
             <section>
                 <article>
                     <span>남은과제</span>
-                    <span>{myProfile.noSubmit}</span>
+                    <span>{myInfo.userEmail}</span>
                 </article>
                 <article>
                     <span>제출한 과제</span>
-                    <span>{myProfile.submit}</span>
+                    <span>{myInfo.userType}</span>
                 </article>
             </section>
         </MyProfileBlock>
