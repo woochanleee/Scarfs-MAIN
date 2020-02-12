fb10e749352a3ff77b168f65d1875d9a5f07c089
leewoochan <030219woo@naver.com>
2020-02-13 05:39:06 +0900

    [MODIFY] <utils> delete

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,20 +1,7 @@
+import React from 'react';
 import axios from 'axios';
+import AuthContext from '../context/Auth';
 const url = "http://54.180.174.253:8888/";
 const ApiDefault = axios.create({ baseURL : url });
 
 export default ApiDefault;
-
-export const IssuingToken = async () => {
-    try {
-        const putAuth = await ApiDefault.put('/auth', undefined, {
-            headers: { Authorization: localStorage.getItem('refresh_token') }
-        })
-        console.log(putAuth);
-        localStorage.setItem('access_token', putAuth.data.accessToken);
-        localStorage.setItem('refresh_token', putAuth.data.refreshToken);
-        return 'success';
-    } catch (e) {
-        console.log(e);
-        throw new Error(e.response.status);
-    }
-};
